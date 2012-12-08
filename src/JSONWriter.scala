f48e913669a72087bf9e0d372a4aed2b232ef553
Paul Asmuth <paul@paulasmuth.com>
2012-12-08 02:49:59 +0100

    appending to the request resp_data string is slooow. stringbuilder is way faster (200ms vs 0.8ms)

--- src/JSONWriter.scala
+++ src/JSONWriter.scala
@@ -5,8 +5,12 @@ class JSONWriter(req: Request) {
   val INDENT = "  "
   var ind = 0
 
-  def run : Unit =
+  val buf = new StringBuffer
+
+  def run : Unit = {
     next(req.stack.root)
+    req.resp_data = buf.toString
+  }
 
   private def next(cur: Instruction) : Unit = {
     var s : (() => Unit) = null
@@ -14,15 +18,13 @@ class JSONWriter(req: Request) {
     cur.name match {
 
       case "execute" =>
-        if (cur == req.stack.root) s = scope((("{","}")))
+        { indent; s = scope((("{","}"))) }
 
       case "findSingle" =>
-        { write(json(cur.relation.name) + ":"); s = scope((("{","}"))) } 
+        { write(json(cur.relation.name) + ": "); s = scope((("{","}"))) } 
 
       case "findMulti" =>
-        { write(json(cur.relation.name) + ":"); s = scope((("[","]"))) }
-
-      case _ => null
+        { write(json(cur.relation.name) + ": "); s = scope((("[","]"))) }
 
     }
 
@@ -31,17 +33,20 @@ class JSONWriter(req: Request) {
 
     if (s != null) s()
 
-    write(",\n")
+    buf.append(",\n")
   }
 
   private def json(str: String) : String =
     "\"" + str.replaceAll("\"", "\\\"") + "\""
 
   private def write(str: String) : Unit =
-    if (str != null) req.resp_data += (INDENT * ind) + str
+    if (str != null) buf.append((INDENT * ind) + str)
 
   private def scope(s: (String, String)) : (() => Unit) =
-    { req.resp_data += " " + s._1 + "\n"; ind += 1; (() =>
+    { buf.append(s._1 + "\n"); ind += 1; (() =>
       { ind -= 1; write(s._2) })}
+
+  private def indent : Unit =
+    buf.append(INDENT * ind)
 
 }
