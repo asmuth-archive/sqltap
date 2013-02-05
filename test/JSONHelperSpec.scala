0da5c11b8d49f8d79c05e9a9d891e7c250767dbe
Anton S <anton.smyschlajew@dawandamail.com>
2013-02-05 14:35:32 +0100

    escape control and other unprintable characters in json

--- test/JSONHelperSpec.scala
+++ test/JSONHelperSpec.scala
@@ -8,6 +8,12 @@ class JSONHelperSpec extends FlatSpec {
   behavior of "JSONHelper.escape"
 
 
+  it should "escape backslashes" in {  
+    val input = "\\"
+    val output = JSONHelper.escape(input)
+    assert(output === """\\""")
+  }
+
   it should "escape double quotes" in {  
     val input = "\""
     val output = JSONHelper.escape(input)
@@ -20,15 +26,34 @@ class JSONHelperSpec extends FlatSpec {
     assert(output === "")
   }
 
-  it should "convert newlines" in {  
+  it should "escape newlines" in {  
     val input = "\n"
     val output = JSONHelper.escape(input)
     assert(output === """\n""")
   }
 
-  it should "convert vertical tabs" in {  
-    val input = "\n\t"
+  it should "escape tabs" in {  
+    val input = "\t"
     val output = JSONHelper.escape(input)
-    assert(output === """\n""")
+    assert(output === """\t""")
   }
+  
+  it should "strip unprintable characters" in {  
+    val input = List(5,14,127).map(c => c.toChar).mkString
+    val output = JSONHelper.escape(input)
+    assert(output === "")
+  }
+
+  it should "convert VT and FF characters to whitespaces" in {  
+    val input = List(11,12).map(c => c.toChar).mkString
+    val output = JSONHelper.escape(input)
+    assert(output === " "*2)
+  }
+
+  it should "convert separator characters to whitespaces" in {  
+    val input = List.range(28,32).map(c => c.toChar).mkString
+    val output = JSONHelper.escape(input)
+    assert(output === " "*4)
+  }
+
 }
