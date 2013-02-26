866475703675167879d6d566e83c131ef36e2216
Paul Asmuth <paul@paulasmuth.com>
2013-02-26 13:51:03 +0100

    gzip data before putting it into memcache

--- src/PreparedQueryCache.scala
+++ src/PreparedQueryCache.scala
@@ -1,11 +1,15 @@
 package com.paulasmuth.sqltap
 
 import java.util.logging._
-import net.spy.memcached.MemcachedClient;
-import net.spy.memcached.AddrUtil;
-import net.spy.memcached.transcoders.Transcoder;
-import net.spy.memcached.CachedData;
-import java.util.concurrent._;
+import net.spy.memcached.MemcachedClient
+import net.spy.memcached.AddrUtil
+import net.spy.memcached.transcoders.Transcoder
+import net.spy.memcached.CachedData
+import java.util.concurrent._
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
 
 object PreparedQueryCache {
   System.setProperty("net.spy.log.LoggerImpl",
@@ -15,14 +19,47 @@ object PreparedQueryCache {
     .setLevel(Level.SEVERE);
 
   class PlainMemcachedTranscoder extends Transcoder[String] {
-    def getMaxSize : Int = Integer.MAX_VALUE
+    def getMaxSize : Int = 4096 * 8
     def asyncDecode(d: CachedData) : Boolean = true
 
-    def encode(s: String) : CachedData =
-      new CachedData(0, s.getBytes("UTF-8"), getMaxSize)
+    def encode(s: String) : CachedData = {
+      val target = new ByteArrayOutputStream()
+      val gzip   = new GZIPOutputStream(target)
+      val data   = s.getBytes("UTF-8")
 
-    def decode(d: CachedData) : String = {
-      new String(d.getData, "UTF-8")
+      gzip.write(data, 0, data.size)
+      gzip.finish
+      gzip.close
+
+      target.close
+
+      new CachedData(3, target.toByteArray, getMaxSize);
+    }
+
+    def decode(d: CachedData) : String = try {
+      val buffer = new Array[Byte](getMaxSize)
+      val source = new ByteArrayInputStream(d.getData);
+      val gzip   = new GZIPInputStream(source);
+
+      var read_len = 0
+      var read = 0
+
+      while (read >= 0) {
+        read = gzip.read(buffer, read_len, buffer.size - read_len)
+
+        if (read > 0)
+          read_len += read
+      }
+
+      gzip.close
+      source.close
+
+      new String(buffer, 0, read_len, "UTF-8")
+    } catch {
+      case e: Exception => {
+        SQLTap.error("[MEMCACHE] " + e, false)
+        return "error"
+      }
     }
 
   }
