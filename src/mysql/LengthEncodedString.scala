3e513ef0e0098099e82d57b958951fa3afd4bab0
Paul Asmuth <paul@paulasmuth.com>
2013-05-25 17:04:48 +0200

    read mysql LengthEncodedString

---
+++ src/mysql/LengthEncodedString.scala
@@ -0,0 +1,43 @@
+// This file is part of the "SQLTap" project
+//   (c) 2011-2013 Paul Asmuth <paul@paulasmuth.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+package com.paulasmuth.sqltap.mysql
+
+object LengthEncodedString {
+
+  def read(data: Array[Byte], pos: Int) : String = {
+    var length : Int = 0
+    var offset : Int = pos
+
+    if ((data(pos) & 0x000000ff) < 0xfb) {
+      length += (data(0) & 0x000000ff)
+      offset += 1
+    }
+
+    else if ((data(pos) & 0x000000ff) == 0xfc) {
+      length += (data(pos + 1) & 0x000000ff)
+      length += (data(pos + 2) & 0x000000ff) << 8
+      offset += 3
+    }
+
+    else if ((data(0) & 0x000000ff) == 0xfd) {
+      length += (data(pos + 1) & 0x000000ff)
+      length += (data(pos + 2) & 0x000000ff) << 8
+      length += (data(pos + 3) & 0x000000ff) << 16
+      offset += 4
+    }
+
+    else if ((data(0) & 0x000000ff) == 0xfe && data.size == 9)
+      throw new SQLProtocolError("length encoded string too large!")
+
+    else
+      throw new SQLProtocolError("invalid length encoded string")
+
+    return new String(data, offset, length, "UTF-8")
+  }
+
+}
