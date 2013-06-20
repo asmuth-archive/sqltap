f46e1078d51c4a7252be9702aa53dfd3a0430824
Paul Asmuth <paul@paulasmuth.com>
2013-06-20 15:22:46 +0200

    version bump to 0.4.1

--- src/SQLTap.scala
+++ src/SQLTap.scala
@@ -15,7 +15,7 @@ import scala.collection.mutable.HashMap;
 
 object SQLTap{
 
-  val VERSION = "v0.4.0"
+  val VERSION = "v0.4.1"
   val CONFIG  = HashMap[Symbol,String]()
 
   var DEFAULTS = HashMap[Symbol, String](
