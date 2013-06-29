a7fd35f799835348d6a267e1ef1eec608678666c
Paul Asmuth <paul@paulasmuth.com>
2013-06-29 17:45:27 +0200

    proper states for FindSingleInstruction

--- src/FindSingleInstruction.scala
+++ src/FindSingleInstruction.scala
@@ -11,71 +11,83 @@ import com.paulasmuth.sqltap.mysql.{SQLQuery}
 
 class FindSingleInstruction extends SQLInstruction with CTreeInstruction {
 
+  val INS_STATE_INIT = 1
+  val INS_STATE_READY = 2
+  val INS_STATE_CTREE = 3
+  val INS_STATE_QREADY = 4
+  val INS_STATE_QUERY = 5
+  val INS_STATE_DONE = 6
+
   val name = "findSingle"
+  var state = INS_STATE_INIT
 
   var conditions : String = null
   var order      : String = null
 
-  def execute(_worker: Worker) : Unit = {
-    var skip       : Boolean = false
-    var join_field : String  = null
-    var join_id    : Int     = 0
+  var join_field : String  = null
+  var join_id    : Int     = 0
 
+  def execute(_worker: Worker) : Unit = {
     worker = _worker
 
-    if (finished || ctree_wait)
-      return
+    state match {
 
-    if (record_id != null)
-      record.set_id(record_id)
+      case INS_STATE_INIT => {
+        if (record_id != null)
+          record.set_id(record_id)
 
-    if (!fields.contains(record.resource.id_field))
-      fields += record.resource.id_field
+        if (!fields.contains(record.resource.id_field))
+          fields += record.resource.id_field
 
-    if (record.has_id) {
-      // optimization: skip select id from ... where id = ...; queries
-      if (fields.length == 1 && fields.head == record.resource.id_field) {
-        skip = true
+        if (record.has_id) {
+          state = INS_STATE_READY
+
+          join_field = relation.resource.id_field
+          join_id = record.id
+        }
+
+        else if (relation.join_foreign == false && prev.is_finished) {
+          val join_id_str = prev.record.get(relation.join_field)
+
+          if (join_id_str == null) {
+            state = INS_STATE_DONE
+            next.clear()
+            return cancel(worker)
+          }
+
+          state = INS_STATE_READY
+          join_id = join_id_str.toInt
+
+          if (relation.join_field_remote != null) {
+            join_field = relation.join_field_remote
+          } else {
+            join_field = relation.resource.id_field
+            record.set_id(join_id)
+          }
+        }
+
+        else if (relation.join_foreign == true && prev.record.has_id) {
+          state = INS_STATE_READY
+          join_field = relation.join_field
+          join_id = prev.record.id
+        }
+
+        if (state == INS_STATE_READY) {
+          execute(worker)
+        } else if (prev.is_finished) {
+          throw new ExecutionException("deadlock detected")
+        }
       }
 
-      join_field = relation.resource.id_field
-      join_id = record.id
-    }
-
-    else if (relation.join_foreign == false && prev.is_finished) {
-      val join_id_str = prev.record.get(relation.join_field)
-
-      if (join_id_str == null) {
-        next.clear()
-        return cancel(worker)
-      }
-
-      join_id = join_id_str.toInt
-
-      if (relation.join_field_remote != null) {
-        join_field = relation.join_field_remote
-      } else {
-        join_field = relation.resource.id_field
-        record.set_id(join_id)
-      }
-    }
-
-    else if (relation.join_foreign == true && prev.record.has_id) {
-      join_field = relation.join_field
-      join_id = prev.record.id
-    }
-
-    else if (prev.is_finished) {
-      throw new ExecutionException("deadlock detected")
-    }
-
-    if (join_field != null) {
-      if (ctree_try) {
-        ctree_try = false
-
+      case INS_STATE_READY => {
         CTreeIndex.find(this) match {
-          case None => ()
+          case None => {
+            state = INS_STATE_QREADY
+            execute(worker)
+          }
           case Some((_ctree, cost)) => {
+            state = INS_STATE_CTREE
+
             ctree      = _ctree
             ctree_wait = true
             ctree_cost = cost
@@ -87,13 +99,25 @@ class FindSingleInstruction extends SQLInstruction with CTreeInstruction {
         }
       }
 
-      if (skip)
-        return cancel(worker)
+      case INS_STATE_QREADY => {
+        // optimization: skip select id from ... where id = ...; queries
+        if (fields.length == 1 && fields.head == record.resource.id_field) {
+          state = INS_STATE_DONE
+          return cancel(worker)
+        }
 
-      execute_query(worker,
-        SQLBuilder.select(
-          relation.resource, join_field, join_id, fields.toList, 
-          conditions, order, null, null))
+        execute_query(worker,
+          SQLBuilder.select(
+            relation.resource, join_field, join_id, fields.toList,
+            conditions, order, null, null))
+      }
+
+      case INS_STATE_DONE =>
+        return
+
+      case INS_STATE_CTREE =>
+        return
+
     }
 
     if (running == true)
@@ -101,6 +125,8 @@ class FindSingleInstruction extends SQLInstruction with CTreeInstruction {
   }
 
   def ready(query: SQLQuery) : Unit = {
+    state = INS_STATE_DONE
+
     if (query.rows.length == 0)
       throw new NotFoundException(this)
     else {
@@ -115,5 +141,15 @@ class FindSingleInstruction extends SQLInstruction with CTreeInstruction {
 
     unroll()
   }
+
+  override def ctree_ready(worker: Worker) : Unit = {
+    state = if (finished)
+      INS_STATE_DONE
+    else
+      INS_STATE_QREADY
+
+    super.ctree_ready(worker)
+  }
+
 
 }
