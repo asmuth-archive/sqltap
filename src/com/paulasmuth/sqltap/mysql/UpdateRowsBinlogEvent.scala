a9defc850533033ba3b36862f1e38b68f3b70ad1
Paul Asmuth <paul@paulasmuth.com>
2013-11-27 14:17:30 +0100

    better bitmap handling

--- src/com/paulasmuth/sqltap/mysql/UpdateRowsBinlogEvent.scala
+++ src/com/paulasmuth/sqltap/mysql/UpdateRowsBinlogEvent.scala
@@ -17,29 +17,34 @@ class UpdateRowsBinlogEvent(data: Array[Byte], ts: Long, fmt: FormatDescriptionB
   val flags        = read_flags
   val extra_data   = read_extra_data
   val num_cols     = read_num_cols
-  val column_map1  = read_column_map
-  val column_set1  = Integer.bitCount(column_map1)
-  val column_map2  = read_column_map
-  val column_set2  = Integer.bitCount(column_map2)
+  val column_map1  = read_bitmap(num_cols)
+  val column_set1  = bitmap_count(num_cols, column_map1)
+  val column_map2  = read_bitmap(num_cols)
+  val column_set2  = bitmap_count(num_cols, column_map2)
 
   def load(table: TableMapBinlogEvent) : Unit = {
-    val null_bitmap = read_int((column_set1 + 7) / 8)
+    println(data.map{ x => x & 0x000000ff}.toList)
 
-    val row = for (col <- 0 until num_cols) yield
-      load_column(col, table.column_types(col))
+    val null_bitmap = read_bitmap(column_set1)
 
-    println(table.table_name, table.schema_name, row)
+    val row = for (col <- 0 until num_cols) {
+      val column_value = load_column(col, table.column_types(col))
+      println("READ COL", col, table.column_types(col), cur, column_value)
+    }
   }
 
   private def load_column(col: Int, column_type: Byte) : String = {
-    if ((column_map1 & (1 << col)) == 0) {
+    if (!bitmap_test(column_map1, col)) {
       return null
     }
 
     column_type match {
       case 0x01 => read_int(1).toString
       case 0x03 => read_int(4).toString
-
+      case 0x07 => read_date()
+      case 0x0a => read_date()
+      case 0x0c => read_date()
+      case 0x0f => read_varchar()
       case c: Byte => {
         throw new Exception("unknown mysql column type: " + c.toString)
       }
@@ -78,17 +83,40 @@ class UpdateRowsBinlogEvent(data: Array[Byte], ts: Long, fmt: FormatDescriptionB
     num._1
   }
 
-  private def read_column_map() : Int = {
-    val len = (num_cols + 7) / 8
-    val map = BinaryInteger.read(data, cur, len)
-    cur    += len
-    map
-  }
-
   private def read_int(bytes: Int) : Int = {
     val num = BinaryInteger.read(data, cur, bytes)
     cur += bytes
     num
+  }
+
+  private def read_date() : String = {
+    val len = BinaryInteger.read(data, cur, 1)
+    cur    += 1 + len
+    "date"
+  }
+
+  private def read_varchar() : String = {
+    val str = LengthEncodedString.read(data, cur)
+    println(data(cur), str._1)
+    cur     = str._2 + 1
+    str._1
+  }
+
+  private def read_bitmap(len: Int) : Integer = {
+    val old = cur
+    cur    += (len + 7) / 8
+    old
+  }
+
+  private def bitmap_count(len: Int, pos: Int) : Int = {
+    (0 until (len + 7) / 8).foldLeft(0) {
+      (m, c) => m + Integer.bitCount(data(pos + c) & 0x000000ff)
+    }
+  }
+
+  private def bitmap_test(pos: Int, bit: Int) : Boolean = {
+    val byte = data(pos + (bit / 8)) & 0x000000ff
+    (byte & (1 << (pos % 8))) > 0
   }
 
 }
