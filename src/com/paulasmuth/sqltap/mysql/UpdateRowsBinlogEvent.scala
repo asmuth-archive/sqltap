b09b9cb75c1fd1879eaf2d91728b722c5b6ebb91
Paul Asmuth <paul@paulasmuth.com>
2013-11-28 13:17:11 +0100

    move generic reader code to RowsBinlogEvent

--- src/com/paulasmuth/sqltap/mysql/UpdateRowsBinlogEvent.scala
+++ src/com/paulasmuth/sqltap/mysql/UpdateRowsBinlogEvent.scala
@@ -6,14 +6,13 @@
 // the License at: http://opensource.org/licenses/MIT
 
 package com.paulasmuth.sqltap.mysql
-import scala.collection.mutable.{HashMap,ListBuffer}
+import scala.collection.mutable.{ListBuffer}
 
-class UpdateRowsBinlogEvent(data: Array[Byte], ts: Long, fmt: FormatDescriptionBinlogEvent) extends BinlogEvent {
-  private var cur = 0
-
+class UpdateRowsBinlogEvent(d: Array[Byte], ts: Long, fmt: FormatDescriptionBinlogEvent) extends RowsBinlogEvent {
   var rows         = new ListBuffer[List[String]]()
   val timestamp    = ts
-  val table_id     = read_table_id
+  val data         = d
+  val table_id     = read_table_id(fmt)
   val flags        = read_flags
   val extra_data   = read_extra_data
   val num_cols     = read_num_cols
@@ -37,106 +36,6 @@ class UpdateRowsBinlogEvent(data: Array[Byte], ts: Long, fmt: FormatDescriptionB
         case true  => ((col, load_column(col, table.column_types(col))))
         case false => ((col, null))
       }
-  }
-
-  private def load_column(col: Int, column_type: Byte) : String = {
-    val x = column_type match {
-      case 0x01 => read_int(1).toString       // 0x01 TINY
-      case 0x02 => read_int(2).toString       // 0x02 SHORT
-      case 0x03 => read_int(4).toString       // 0x03 LONG
-      case 0x04 => read_float(4).toString     // 0x04 FLOAT
-      case 0x05 => read_float(8).toString     // 0x05 DOUBLE
-      case 0x06 => null                       // 0x06 NULL
-      case 0x07 => read_int(4).toString       // 0x07 TIMESTAMP
-      case 0x08 => read_int(8).toString       // 0x08 LONGLONG
-      case 0x09 => read_int(3).toString       // 0x09 INT24
-      case 0x0a => read_date(3)               // 0x0a DATE
-      case 0x0b => read_date(3)               // 0x0b TIME
-      case 0x0c => read_date(8)               // 0x0c DATETIME
-      case 0x0d => read_date(1)               // 0x0d YEAR
-      case 0x0f => read_varchar()             // 0x0f VARCHAR
-      case c: Byte => {
-        throw new Exception("unknown mysql column type: " + c.toString)
-      }
-    }
-
-    println("READ COL", cur, col, column_type, x)
-    x
-  }
-
-  private def read_table_id() : Int = {
-    if (fmt.header_length(0x1f) == 6) {
-      cur = 24
-      BinaryInteger.read(data, 20, 4)
-    } else {
-      cur = 26
-      BinaryInteger.read(data, 20, 6)
-    }
-  }
-
-  private def read_flags() : Int = {
-    cur += 2
-    BinaryInteger.read(data, cur - 2, 2)
-  }
-
-  private def read_extra_data() : String = {
-    val len = BinaryInteger.read(data, cur, 2)
-    cur += len
-
-    if (len > 2) {
-      BinaryString.read(data, cur + 2 - len, len - 2)
-    } else {
-      null
-    }
-  }
-
-  private def read_num_cols() : Int = {
-    val num = LengthEncodedInteger.read(data, cur)
-    cur     = num._2
-    num._1
-  }
-
-  private def read_int(bytes: Int) : Int = {
-    val num = BinaryInteger.read(data, cur, bytes)
-    cur += bytes
-    num
-  }
-
-  private def read_date(len: Int) : String = {
-    //println(java.util.Arrays.copyOfRange(data, cur, cur + 11).toList.map{x=>x&0x000000ff})
-    //println("DATE LEN", data(cur - 1) & 0x000000ff)
-    //println("DATE LEN", data(cur) & 0x000000ff)
-    //println("DATE LEN", data(cur + 1) & 0x000000ff)
-    cur    += len
-    "date" // FIXPAUL
-  }
-
-  private def read_varchar() : String = {
-    val str = LengthEncodedString.read(data, cur)
-    cur     = str._2
-    str._1
-  }
-
-  private def read_float(len: Int) : Double = {
-    val value = read_int(len)
-    java.lang.Double.longBitsToDouble(value) // FIXPAUL
-  }
-
-  private def read_bitmap(len: Int) : Integer = {
-    val old = cur
-    cur    += (len + 7) / 8
-    old
-  }
-
-  private def bitmap_count(len: Int, pos: Int) : Int = {
-    (0 until (len + 7) / 8).foldLeft(0) {
-      (m, c) => m + Integer.bitCount(data(pos + c) & 0x000000ff)
-    }
-  }
-
-  private def bitmap_test(pos: Int, bit: Int) : Boolean = {
-    val byte = data(pos + (bit / 8)) & 0x000000ff
-    (byte & (1 << (bit % 8))) > 0
   }
 
 }
